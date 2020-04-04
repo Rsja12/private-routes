7da7c02520899faca7d571635bda843d094dbb96
Rsja12 <asocarras10@gmail.com>
2020-04-04 15:27:58 -0400

    creates private and public components

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,18 @@
 import React, { Component } from 'react'
+import { BrowserRouter as Router, Route, Link, Redirect } from 'react-router-dom'
+
+
+const fakeAuth = {
+    isAuthenticated: false,
+    authenticate(cb) {
+        this.isAuthenticated = true
+        setTimeout(cb, 100) // fake async
+    },
+    signout(cb) {
+        this.isAuthenticated = false
+        setTimeout(cb, 100) // fake async 
+    }
+}
 
 export class App extends Component {
     render() {
