2c058c533c7ca4d058256132d1d906a45055f566
Rsja12 <asocarras10@gmail.com>
2020-04-04 15:23:37 -0400

    install react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Route, Link, Redirect } from 'react-router-dom'
 
-import App from '../containers/App'
+import App from './containers/App'
 
 ReactDOM.render(<App />, document.querySelector('#root'))
