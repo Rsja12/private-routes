ab6785be1377b94a9a15b4ad7ec5db334e04abbe
Rsja12 <asocarras10@gmail.com>
2020-04-04 15:41:41 -0400

    create privateRoutes

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Route, Link, Redirect } from 'react-router-dom'
 
+import Public from '../components/Public'
+import Login from '../components/Login'
+import Private from '../components/Private'
 
 const fakeAuth = {
     isAuthenticated: false,
@@ -14,12 +17,29 @@ const fakeAuth = {
     }
 }
 
+const PrivateRoute = ({ component: Component, ...rest }) => (
+    <Route {...rest} render={ (props) => (
+        fakeAuth.isAuthenticated === true ? <Component {...props} /> : <Redirect to="login" />
+    )} />
+)
+
 export class App extends Component {
     render() {
         return (
-            <div>
-                React Router 
-            </div>
+            <Router>
+                <div>
+                    <ul>
+                        <li><Link to="/public">Public</Link></li>
+                        <li><Link to="/private">Private</Link></li>
+                    </ul>
+
+                    <Route path="/public" component={Public} />
+
+                    <Route path="/login" component={Login} />
+
+                    <PrivateRoute path="/private" component={Private} />
+                </div>
+            </Router>
         )
     }
 }
